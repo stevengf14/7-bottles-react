7e5548ce689a34d7d0d44ed41012113b3788a052
Steven <stevengf14@gmail.com>
2021-10-07 17:41:36 -0500

    N - Component Result

---
+++ src/components/Result.js
@@ -0,0 +1,17 @@
+import Matrix from "./Matrix"
+
+export default function Result(props) {
+    const { data } = props
+    return (
+        <div>
+            <div className="column">
+                <div className="column is-quarter"><strong>Iteration: </strong>{data.iteration}</div>
+                <div className="column is-quarter"><strong>Individual: </strong>{data.individual}</div>
+                <div className="column is-half"><strong>Cost: </strong>{data.cost}</div>
+                <div className="column is-half"><strong>Best Solution: </strong>
+                    <Matrix data={data.bestSolution} />
+                </div>
+            </div>
+        </div>
+    )
+}
